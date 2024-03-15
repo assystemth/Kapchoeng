0e287ed816d60f55b2eca61c66d6e28530053127
assystemth <dev@assystem.co.th>
2024-03-15 17:53:34 +0700

    ฟฟฟ

--- application/views/frontend_templat/footer.php
+++ application/views/frontend_templat/footer.php
@@ -2,11 +2,19 @@
 
 <footer class="footer">
     <div class="credit">
-        <span class="font-footer">
-            เทศบาลตำบลกาบเชิง เลขที่ 39 หมู่ที่ 17 ตำบลกาบเชิง อำเภอกาบเชิง จังหวัดสุรินทร์ 32210<br>
-            เบอร์โทรศัพท์ 0-4455-9143<br>
-            &copy; สงวนลิขสิทธิ์ 2565 โดย <a href="https://www.assystem.co.th/" target="_blank">บริษัท เอเอส ซิสเต็ม จำกัด</a>&nbsp;&nbsp;โทร 084-393-5580
-        </span>
+        <div class="row">
+            <div class="col-8">
+                <span class="font-footer">
+                    เลขที่ 39 หมู่ที่ 17 ตำบลกาบเชิง อำเภอกาบเชิง จังหวัดสุรินทร์ 32210<br>
+                    เบอร์โทรศัพท์ 0-4455-9143<br>
+                    &copy; สงวนลิขสิทธิ์ 2565 โดย <a href="https://www.assystem.co.th/" target="_blank">บริษัท เอเอส ซิสเต็ม จำกัด</a>&nbsp;&nbsp;โทร 084-393-5580
+                </span>
+            </div>
+            <div class="col-4" style="margin-top: -25px;">
+                <iframe width="266" height="147" frameborder="0" src="https://www.google.com/maps?q=สำนักงานเทศบาลตำบลกาบเชิง&output=embed" allowfullscreen style="border-radius: 24px;"></iframe>
+            </div>
+        </div>
+
     </div>
 </footer>
 </main>
